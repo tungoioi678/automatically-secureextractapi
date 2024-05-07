40ed913f897c243a69a3e03699fc6d4d4fbabb3d
tungoioi678 <givision10pun0suy@hotmail.com>
2024-05-07 10:45:43 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-function canFinish(numCourses, prerequisites) {
-  const graph = new Map();
-  const visited = new Array(numCourses).fill(0);
-  for (const [course, prerequisite] of prerequisites) {
-    if (!graph.has(course)) graph.set(course, []);
-    graph.get(course).push(prerequisite);
+const gcdRecursive = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  for (let i = 0; i < numCourses; i++) {
-    if (!dfs(i)) return false;
-  }
-  return true;
-  function dfs(course) {
-    if (visited[course] === 1) return false;
-    if (visited[course] === -1) return true;
-    visited[course] = 1;
-    if (graph.has(course)) {
-      for (const prerequisite of graph.get(course)) {
-        if (!dfs(prerequisite)) return false;
-      }
-    }
-    visited[course] = -1;
-    return true;
-  }
-}
+  return gcdRecursive(b, a % b);
+};
