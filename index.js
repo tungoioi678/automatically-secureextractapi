c10476af99dd47a49e47448afc974a6e43cf85ea
tungoioi678 <givision10pun0suy@hotmail.com>
2024-05-09 13:44:31 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
-  }
-  return i === s.length;
+function countVowels(str) {
+  return str.match(/[aeiou]/gi).length;
 }
