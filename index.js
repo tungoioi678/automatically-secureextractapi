68f4f43657e3070e444714839af22d0ec803f5ae
tungoioi678 <givision10pun0suy@hotmail.com>
2024-05-10 18:42:27 +0700

    start

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function countVowels(str) {
-  return str.match(/[aeiou]/gi).length;
+function countPrimes(n) {
+  const isPrime = new Array(n).fill(true);
+  isPrime[0] = false;
+  isPrime[1] = false;
+  for (let i = 2; i * i < n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j < n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (isPrime[i]) count++;
+  }
+  return count;
 }
